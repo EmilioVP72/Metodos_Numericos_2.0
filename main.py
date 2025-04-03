622a13b04277562f235867cf9ab18c692b55f9a0
Alessandro Hernandez Sanchez <lugoraptor1@gmail.com>
2025-04-03 12:38:48 -0600

    ahi está su puto push para que no ande chingando el giganigga del serch (añadidos método de jacobi y regla falsa)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from Gauss_Jordan import GaussJordanApp
 from Newton_Raphson import NewtonRaphsonApp  
+from Jacobi import JacobiApp
+from Regla_Falsa import ReglaFalsaApp
 
 def open_gauss_jordan():
     gauss_root = tk.Toplevel()
@@ -11,6 +13,16 @@ def open_newton_raphson():
     newton_root = tk.Toplevel()
     newton_app = NewtonRaphsonApp(newton_root)
     newton_root.mainloop()
+
+def open_jacobi():
+    jacobi_root = tk.Toplevel()
+    jacobi_app = JacobiApp(jacobi_root)
+    jacobi_root.mainloop()
+
+def open_regla_falsa():
+    regla_falsa_root = tk.Toplevel()
+    regla_falsa_root = ReglaFalsaApp(regla_falsa_root)
+    regla_falsa_root.mainloop()
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -24,5 +36,11 @@ if __name__ == "__main__":
     
     btn_newton = tk.Button(root, text="Newton-Raphson", command=open_newton_raphson)
     btn_newton.pack(pady=5)
+    
+    btn_jacobi = tk.Button(root, text = "Jacobi", command = open_jacobi)
+    btn_jacobi.pack(pady = 5)
+
+    btn_rf = tk.Button(root, text = "Regla Falsa", command = open_jacobi)
+    btn_rf.pack(pady = 5)
     
     root.mainloop()
